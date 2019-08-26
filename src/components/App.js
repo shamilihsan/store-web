efc2eb0d3a66cf935c7b65d7b9c4f66a7b4e3e03
Shamil Ihsan <shamil.ihsan@hotmail.com>
2019-08-26 20:05:28 +0530

    Initial clean up

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
 
-import StreamCreate from './streams/StreamCreate'
-import StreamEdit from './streams/StreamEdit'
-import StreamDelete from './streams/StreamDelete'
-import StreamList from './streams/StreamList'
-import StreamShow from './streams/StreamShow'
+import Home from './Home'
 import Header from './Header'
 import history from '../history'
 
@@ -16,11 +12,7 @@ const App = () => {
             <Router history={history}>
             <Header />
                 <Switch>
-                    <Route path="/" exact component={StreamList}></Route>
-                    <Route path="/streams/new" exact component={StreamCreate}></Route>
-                    <Route path="/streams/edit/:id" exact component={StreamEdit}></Route>
-                    <Route path="/streams/delete/:id" exact component={StreamDelete}></Route>
-                    <Route path="/streams/:id" exact component={StreamShow}></Route>
+                    <Route path="/" exact component={Home}></Route>
                 </Switch>
             </Router>
         </div>
