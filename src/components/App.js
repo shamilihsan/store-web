023ba2684deca62eaff7c6890d46e13f085b9002
Shamil Ihsan <shamil.ihsan@hotmail.com>
2019-08-29 17:47:23 +0530

    Added routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
 
 import Home from './Home'
+import Item from './Item'
 import Header from './Header'
 import history from '../history'
 
@@ -12,6 +13,7 @@ const App = () => {
             <Router history={history}>
             <Header />
                 <Switch>
+                    <Route path="/shop/:id" exact component={Item}></Route> 
                     <Route path="/" exact component={Home}></Route>
                 </Switch>
             </Router>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 class Item extends React.Component {
-    
+    render() {
+        return (
+            <h1 className="text-center">Item</h1>
+        )
+    }
 }
 
 export default Item
