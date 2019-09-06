13089bbda880552f8b5ac811239d6c00b988dd0d
Shamil Ihsan <shamil.ihsan@hotmail.com>
2019-09-06 14:18:20 +0530

    Added cart funtionality

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { getItem } from '../actions'
-import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button
-} from 'reactstrap';
 
 class Item extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            total: 0,
+            cartItems: []
+        }
+    }
+
     componentDidMount() {
         this.props.getItem(this.props.match.params.id);
+    }
+
+    addToCart = () => {
+        const { item } = this.props;
+
+        if (localStorage.getItem('cart') == null) {
+            //Add a new cart
+            let cart = [];
+            cart.push({ item: item, quantity: 1 })
+            localStorage.setItem('cart', JSON.stringify(cart))
+        } else {
+            let cart = JSON.parse(localStorage.getItem('cart'))
+            //Find index where item already exists
+            const cartItemIndex = cart.findIndex(cp => {
+                return cp.item._id.toString() === item._id.toString()
+            })
+            let newQuantity = 1
+            const updatedCartItems = JSON.parse(localStorage.getItem('cart'));
+            //If item exists increase quantity, else add new item to cart
+            if (cartItemIndex >= 0) {
+                newQuantity = updatedCartItems[cartItemIndex].quantity + 1
+                updatedCartItems[cartItemIndex].quantity = newQuantity
+            } else {
+                updatedCartItems.push({
+                    item: item,
+                    quantity: newQuantity
+                })
+            }
+            localStorage.setItem('cart', JSON.stringify(updatedCartItems))
+        }
+
+        this.loadCart();
+    }
+
+    loadCart = () => {
+        console.log(JSON.parse(localStorage.getItem('cart')))
     }
 
     render() {
@@ -23,17 +63,15 @@ class Item extends React.Component {
 
         return (
             <React.Fragment>
-                <h1 className="text-center" style={{paddingTop: '50px'}}>{item.name}</h1>
-                <div style={{ paddingTop: '50px', maxWidth: '400px' }}>
-                    <Card>
-                        <CardImg top width="100%" src="https://dummyimage.com/600x400/000/fff" alt="Card image cap" />
-                        <CardBody>
-                            <CardTitle>{item.name}</CardTitle>
-                            <CardSubtitle>{item.description}</CardSubtitle>
-                            <CardText>{item.price}</CardText>
-                            <Button>Button</Button>
-                        </CardBody>
-                    </Card>
+                <h1 className="text-center" style={{ paddingTop: '50px' }}>{item.name}</h1>
+                <div className="card" style={{ width: '18rem' }}>
+                    <img src="https://dummyimage.com/600x400/000/fff" className="card-img-top" />
+                    <div className="card-body">
+                        <h5 className="card-title">{item.name}</h5>
+                        <h5 className="card-title">{item.description}</h5>
+                        <p className="card-text">{item.price}</p>
+                        <a onClick={this.addToCart} className="btn btn-primary text-white">Add to Cart</a>
+                    </div>
                 </div>
             </React.Fragment>
         )
